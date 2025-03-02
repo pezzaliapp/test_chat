2ffba45bbedcc1295d17b9763280ad7bee38c67f
pezzaliapp <pezzalialessandro@gmail.com>
2025-03-02 21:45:04 +0100

    Fix WebSocket issues with gevent

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ from flask_socketio import SocketIO, send
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'questa_e_una_chiave_segreta'
 
-# Forziamo l'utilizzo di gevent come async mode (se usi gevent)
-socketio = SocketIO(app, cors_allowed_origins="*", async_mode="gevent")
+# Usa gevent come modalità asincrona
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode="gevent", logger=True, engineio_logger=True)
 
 @app.route('/')
 def index():
@@ -18,6 +18,6 @@ def handle_message(msg):
     send(msg, broadcast=True)
 
 if __name__ == '__main__':
-    # Usa una porta diversa, ad esempio 5001
-    port = int(os.environ.get("PORT", 5001))
+    # Render assegna automaticamente una porta
+    port = int(os.environ.get("PORT", 5000))
     socketio.run(app, host='0.0.0.0', port=port, debug=True)
